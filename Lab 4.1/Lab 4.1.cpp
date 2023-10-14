f1629e377e20d7f495e9e9e87227ca4c5f4c12b9
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-10-14 15:50:32 +0300

    Add project files.

---
+++ Lab 4.1/Lab 4.1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+	int N, k;
+	double sum = 0;
+	cout << "N: "; cin >> N;
+	k = N;
+	while (k <= 19) {
+		sum += sqrt(pow(sin(k), 2)) + pow(cos(N/k), 2);
+		k++;
+	}
+	cout << "sum: " << sum << endl;
+	sum = 0;
+
+	k = N;
+	do {
+		sum += sqrt(pow(sin(k), 2)) + pow(cos(N / k), 2);
+		k++;
+	} while (k <= 19);
+	cout << "sum: " << sum << endl;
+	sum = 0;
+
+	for (int k = N; k <= 19; k++) {
+		sum += sqrt(pow(sin(k), 2)) + pow(cos(N / k), 2);
+	}
+	cout << "sum: " << sum << endl;
+	sum = 0;
+
+	for (int k = 19; k >= N; k--) {
+		sum += sqrt(pow(sin(k), 2)) + pow(cos(N / k), 2);
+	}
+	cout << "sum: " << sum << endl;
+
+	return 0;
+}
